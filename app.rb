77622aa7262c0d70a997f3045cf99abb8b85bf1a
Regina Jackson <rjackson156@gmail.com>
2021-12-12 21:12:19 -0500

    Move functionality to home page

--- app.rb
+++ app.rb
@@ -6,32 +6,9 @@ get '/' do
   erb :home
 end
 
-get '/food' do
-  erb :food
-end
-
-post '/food' do
+post'/' do
   uri = URI.parse(URI.encode("https://is-vegan.netlify.app/.netlify/functions/api?ingredients=#{params[:ingredient]}"))
   api_response = Net::HTTP.get(uri)
   parsed_response = JSON.parse(api_response)
-  erb :food, locals:{ valid: parsed_response["isVeganSafe"].to_s }
+  erb :home, locals:{ valid: parsed_response["isVeganSafe"].to_s }
 end
-
-=begin
-get '/about' do
-  erb :about
-end
-
-get '/contact' do
-  erb :contact
-end
-
-get '/search' do
-  erb :search
-end
-
-# This page gets rendered when the user enters an unidentified route
-not_found do
-  erb :not_found, :layout => :error_layout
-end
-=end
